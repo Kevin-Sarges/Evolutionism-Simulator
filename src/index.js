ec82d89de00e485cc0c346ae886108fa4f18a3e6
Diogo Felipe <dlcfelipp@gmail.com>
2020-09-25 12:01:57 -0300

    Form insert

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-let simulation = new Simulation(100, 100, 10, 100);
+let button = document.getElementById('simulate');
 
-simulation.simulate();
+let numberOfSimulatedDaysInput = document.getElementById('number-of-simulated-days');
+let populationBaseEnergyInput = document.getElementById('population-base-energy');
+let initialIndividualNumberInput = document.getElementById('initial-individual-number');
+let amountOfFoodInput = document.getElementById('amount-of-food');
+
+button.addEventListener('click', simulate);
+
+function simulate(e){
+    e.preventDefault();
+
+    let numberOfSimulatedDays = numberOfSimulatedDaysInput.value;
+    let populationBaseEnergy = populationBaseEnergyInput.value;
+    let initialIndividualNumber = initialIndividualNumberInput.value;
+    let amountOfFood = amountOfFoodInput.value;
+
+    let simulation = new Simulation(numberOfSimulatedDays, populationBaseEnergy, initialIndividualNumber, amountOfFood);
+
+    button.disabled = true;
+
+    simulation.simulate().then(()=>{
+        button.disabled = false;
+    });
+}
